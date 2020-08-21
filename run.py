66ec2aaaf474806c6de64f714e33fc029de59771
LYXcode <1367527584@qq.com>
2020-08-21 19:48:25 +0800

    original test

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 import os
 import trimesh
-
-
+from trimesh import transformations as tf
+import math
 models = os.listdir('./Models')
 
+transformAngles = []
+quater = tf.quaternion_from_euler(0, 0, math.pi)
+matrix = tf.quaternion_matrix(quater)
 for model in models:
-
     modelName = model.split('.')[0]    
     mesh = trimesh.load('./Models/'+ model)
-    mesh.apply_transform(trimesh.transformations.random_rotation_matrix())
     path = './ModelObj/'+ modelName + '.obj'
     mesh.export(path)   
     os.system('./Binaries/cup_objectbased.exe ' + path)
