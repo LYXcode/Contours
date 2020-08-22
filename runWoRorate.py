7449e99ca6803544b18e5eb6af10e5d44d44895b
LYXcode <1367527584@qq.com>
2020-08-22 16:19:25 +0800

    succeed

--- runWoRorate.py
+++ runWoRorate.py
@@ -1,20 +1,39 @@
 import os
-import trimesh
-from trimesh import transformations as tf
 import math
 import shutil
-import numpy as np
-
+import bpy
 
 models = os.listdir('./Models')
 shutil.rmtree("./Output", ignore_errors=True)
 os.mkdir("./Output")
 
-for model in models:
-    
-    modelPath = '.\\Models\\' + model
-    os.system('.\\Tool\\cpu_objectbased.exe ' + modelPath)
+D = bpy.data
+C = bpy.context
 
+Angles = [[0,0,0],[0,0,90],[0,0,180],[0,0,270]]
+
+def deg2rad(deg):
+    return deg * math.pi / 180.
+
+def rotate_export_model(filename, x,y,z):
+    path = './Models/' + filename + '.off'
+    bpy.ops.import_mesh.off(filepath=path, filter_glob='*.off')
+    model = D.objects[filename]
+    model.rotation_euler = (deg2rad(x),deg2rad(y), deg2rad(z))
+    model.select_set(True)
+    bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
+    output_path = "./tmp/"+filename+".obj"
+    #bpy.ops.export_mesh.off("EXEC_DEFAULT", filepath=output_path)
+    bpy.ops.export_scene.obj("EXEC_DEFAULT", filepath=output_path, filter_glob="*.obj", use_selection=True)
+    D.objects.remove(model, do_unlink=True)
+    D.meshes.remove(D.meshes[filename])
+
+for model in models:
+    name = model.split('.')[0]
+    for angle in Angles:
+        rotate_export_model(name, angle[0], angle[1], angle[2])
+        modelPath = './tmp/' + name + '.obj'
+        os.system('.\\Tool\\cpu_objectbased.exe ' + modelPath)
 
     files = os.listdir('.')
     for filename in files:
@@ -22,4 +41,5 @@ for model in models:
         if portion[1] == ".ppm":
             newname = filename.replace("img", model.split('.')[0])
             os.rename(filename, newname)
-            shutil.move(newname, ".\\Output")
+            shutil.move(newname, "./Output")
+
